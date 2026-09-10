d6de6d918371eb560e72cc986f7ec38e76214ec1
darth-malu <justinmalu@gmail.com>
2026-09-10 17:39:30 +0300

    Nice work

--- .config/hypr/commonVariables.lua
+++ .config/hypr/commonVariables.lua
@@ -8,19 +8,20 @@ hl.config({
 
 hl.config({
   input = {
-    kb_layout                   = "us",
-    kb_variant                  = "",
-    kb_model                    = "",
-    kb_options                  = "caps:swapescape",
-    kb_rules                    = "",
+    kb_layout          = "us",
+    kb_variant         = "",
+    kb_model           = "",
+    kb_options         = "caps:swapescape",
+    kb_rules           = "",
 
-    numlock_by_default          = true,
+    numlock_by_default = true,
 
-    follow_mouse                = 1,
-    mouse_refocus               = true, -- if true mouse must cross boundary for focus change
+    follow_mouse       = 1,
+    mouse_refocus      = true, -- if true mouse must cross boundary for focus change
 
-    float_switch_override_focus = 2,    -- (1:: or 2), focus will change to the window under the cursor when changing from tiled-to-floating and vice versa. If 2, focus will also follow mouse on float-to-float switches.
-    sensitivity                 = 0,    -- -1.0 - 1.0, 0 means no modification.
+    -- float_switch_override_focus = 2,    -- (1:: or 2), focus will change to the window under the cursor when changing from tiled-to-floating and vice versa. If 2, focus will also follow mouse on float-to-float switches.
+    -- TODO: see if this is what is causing issues with my focusss
+    sensitivity        = 0, -- -1.0 - 1.0, 0 means no modification.
   },
 })
 
